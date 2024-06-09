1ff9dc33fda2552938148c5809b9e8794e861c13
Paul Chavez <paulchavezromero20@gmail.com>
2024-06-09 00:49:31 -0500

    chore: missingSuspenseWithCSRBailout added

--- src/containers/Translate.tsx
+++ src/containers/Translate.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from "react";
 import { LanguagePicker, ToolBelt, TextareaGroup } from "@/components";
 
 export const Translate = () => {
   return (
     <main className="container px-3 max-w-[1000px]">
-      <Suspense>
-        <ToolBelt />
-      </Suspense>
+      <ToolBelt />
       <LanguagePicker />
       <TextareaGroup />
     </main>
