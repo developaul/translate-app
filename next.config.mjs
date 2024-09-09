866f9fb6e6332e109e7614fa2b16e32397de5f64
Paul Chavez <paulchavezromero20@gmail.com>
2024-09-08 20:10:04 -0500

    feat: bundler analyzer added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+export default bundleAnalyzer(nextConfig);
